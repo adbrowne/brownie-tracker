411e722d5779077d7c2d7a5eaa84508922c70a7c
Andrew Browne <brownie@brownie.com.au>
2024-08-30 03:32:19 +1000

    Update script to be non-interactive

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
 yum install make glibc-devel gcc patch
 cargo build --release
 
